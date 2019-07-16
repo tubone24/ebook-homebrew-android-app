ae1171c734b3abfa89fcfcf86b16df35f47b3dc4
tubone24 <tubo.yyyuuu@gmail.com>
2019-07-16 23:39:25 +0900

    add test

--- test/ut/utils/utils_test.dart
+++ test/ut/utils/utils_test.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
+import 'dart:io';
 import 'package:ebook_homebrew_flutter/src/utils/utils.dart';
 
+class MockFile extends Mock implements File {}
+class MockDatetime extends Mock implements DateTime {}
+
 void main() {
+  Utils target = new Utils();
   test('Utils Test convertContentType', (){
-    Utils target = new Utils();
     Map<String, String> testCase = {
       'jpg': 'image/jpeg',
       'png': 'image/png',
@@ -13,5 +18,11 @@ void main() {
       String actual = target.convertContentType(input);
       expect(actual, expected);
     });
+  });
+
+  test('Utils test writeFile', (){
+    File mockFile = new MockFile();
+    when(mockFile.writeAsBytesSync([1]));
+    target.writeFile('test', [1]);
   });
 }
