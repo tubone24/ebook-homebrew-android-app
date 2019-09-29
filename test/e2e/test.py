46bd1303c70eb77d3cb3fc11812feb6befcf6728
tubone24 <tubo.yyyuuu@gmail.com>
2019-09-29 18:39:44 +0900

    appium test

---
+++ test/e2e/test.py
@@ -0,0 +1,43 @@
+import os
+import unittest
+from appium import webdriver
+from time import sleep
+
+class EbookHomebrewAndroidTests(unittest.TestCase):
+    "Class to run tests against the Chess Free app"
+    def setUp(self):
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '9.0'
+        desired_caps['deviceName'] = 'Nexus'
+        # Returns abs path relative to this file and not cwd
+        desired_caps['app'] = os.path.abspath(os.path.join(os.path.dirname(__file__),'../../build/app/outputs/apk/debug/app-debug.apk'))
+        desired_caps['appPackage'] = 'com.tubone.ebook_homebrew_flutter'
+        desired_caps['appActivity'] = '.MainActivity'
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_ui_click(self):
+        sleep(3)
+        el1 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button[1]")
+        el1.click()
+        el2 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]")
+        el2.click()
+        el3 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button[1]")
+        el3.click()
+        el4 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]")
+        el4.click()
+        el5 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button[3]")
+        el5.click()
+        el6 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button")
+        el6.click()
+        el7 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button[4]")
+        el7.click()
+
+
+# ---START OF SCRIPT
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(EbookHomebrewAndroidTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
