ae96b0006741086305c54bb51d79eb442a790b70
tubone24 <tubo.yyyuuu@gmail.com>
2019-12-09 23:55:51 +0900

    fix PEP

--- test/e2e/test.py
+++ test/e2e/test.py
@@ -3,17 +3,15 @@ import unittest
 from appium import webdriver
 from time import sleep
 
+
 class EbookHomebrewAndroidTests(unittest.TestCase):
-    "Class to run tests against the Chess Free app"
+    """Class to run tests against the Chess Free app"""
     def setUp(self):
-        desired_caps = {}
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '9.0'
-        desired_caps['deviceName'] = 'Nexus'
+        desired_caps = {'platformName': 'Android', 'platformVersion': '9.0', 'deviceName': 'Nexus',
+                        'app': os.path.abspath(
+                            os.path.join(os.path.dirname(__file__), '../../build/app/outputs/apk/debug/app-debug.apk')),
+                        'appPackage': 'com.tubone.ebook_homebrew_flutter', 'appActivity': '.MainActivity'}
         # Returns abs path relative to this file and not cwd
-        desired_caps['app'] = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../build/app/outputs/apk/debug/app-debug.apk'))
-        desired_caps['appPackage'] = 'com.tubone.ebook_homebrew_flutter'
-        desired_caps['appActivity'] = '.MainActivity'
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
     def tearDown(self):
@@ -21,19 +19,40 @@ class EbookHomebrewAndroidTests(unittest.TestCase):
 
     def test_ui_click(self):
         sleep(3)
-        el1 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button[1]")
+        el1 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
+                                                "/android.widget.FrameLayout/android.view.View/android.view.View"
+                                                "/android.view.View/android.view.View/android.view.View/android.view"
+                                                ".View/android.view.View[2]/android.widget.Button[1]")
         el1.click()
-        el2 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]")
+        el2 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
+                                                "/android.widget.FrameLayout/android.view.View/android.view.View"
+                                                "/android.view.View/android.view.View/android.view.View/android.view"
+                                                ".View/android.view.View/android.view.View/android.view.View[1]")
         el2.click()
-        el3 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button[1]")
+        el3 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
+                                                "/android.widget.FrameLayout/android.view.View/android.view.View"
+                                                "/android.view.View/android.view.View/android.view.View/android.view"
+                                                ".View/android.view.View[2]/android.widget.Button[1]")
         el3.click()
-        el4 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]")
+        el4 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
+                                                "/android.widget.FrameLayout/android.view.View/android.view.View"
+                                                "/android.view.View/android.view.View/android.view.View/android.view"
+                                                ".View/android.view.View/android.view.View/android.view.View[2]")
         el4.click()
-        el5 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button[3]")
+        el5 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
+                                                "/android.widget.FrameLayout/android.view.View/android.view.View"
+                                                "/android.view.View/android.view.View/android.view.View/android.view"
+                                                ".View/android.view.View[2]/android.widget.Button[3]")
         el5.click()
-        el6 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button")
+        el6 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
+                                                "/android.widget.FrameLayout/android.view.View/android.view.View"
+                                                "/android.view.View/android.view.View/android.view.View/android"
+                                                ".widget.Button")
         el6.click()
-        el7 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button[4]")
+        el7 = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
+                                                "/android.widget.FrameLayout/android.view.View/android.view.View"
+                                                "/android.view.View/android.view.View/android.view.View/android.view"
+                                                ".View/android.view.View[2]/android.widget.Button[4]")
         el7.click()
 
 
